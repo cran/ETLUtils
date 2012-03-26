ca9016834a29d459fd5d7298e1438a1b76ed44e0
Jan Wijffels <jwijffels@bnosac.be>
2012-03-26 00:00:00 +0000

    version 1.0

---
+++ R/ETLUtils-package.R
@@ -0,0 +1,36 @@
+###############################################################################
+## Project: ETLUtils
+## Content: Top level call
+##
+## Author: jan
+## Creation date: Mar 25, 2012, 3:24:33 PM
+## File: ETLUtils-package.R
+###############################################################################
+
+
+
+#' Extra utility functions to execute standard ETL operations on large data
+#'
+#' \tabular{ll}{
+#' Package: \tab ETLUtils\cr
+#' Type: \tab Package\cr
+#' Version: \tab 1.0\cr
+#' Date: \tab 2012-03-26\cr
+#' License: \tab GPL-2\cr
+#' Depends: \tab zoo, DBI, ff\cr
+#' Suggests: \tab RSQLite\cr
+#' LazyLoad: \tab yes\cr
+#' }
+#'
+#' Provides functions to do fast left outer join merging based on match, loading of bigdata (e.g. from Oracle) directly into ffdf objects using DBI and some utility functions like recoding.
+#'
+#'
+#' @name ETLUtils-package
+#' @aliases ETLUtils
+#' @docType package
+#' @title Extra utility functions to execute standard ETL operations on large data
+#' @author Jan Wijffels \email{jwijffels@@bnosac.be}
+#' @keywords package
+#' @examples
+#' # See the specified functions in the package
+
